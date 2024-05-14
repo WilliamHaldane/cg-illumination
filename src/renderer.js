b7123f46e281e57b86a2b97d8b6696a4b8cdc944
William Haldane <hald9449@stthomas.edu>
2024-05-13 20:58:43 -0500

    last changes including custom model, UI movement

--- src/renderer.js
+++ src/renderer.js
@@ -8,6 +8,8 @@ import { Texture } from '@babylonjs/core/Materials/Textures/texture';
 import { RawTexture } from '@babylonjs/core/Materials/Textures/rawTexture';
 import { Color3, Color4 } from '@babylonjs/core/Maths/math.color';
 import { Vector2, Vector3 } from '@babylonjs/core/Maths/math.vector';
+import { VertexData } from '@babylonjs/core/Meshes/mesh.vertexData';
+import { Mesh } from '@babylonjs/core/Meshes/mesh';
 
 const BASE_URL = import.meta.env.BASE_URL || '/';
 
@@ -48,7 +50,7 @@ class Renderer {
                 ambient: new Color3(0.4, 0.2, 0.2),
                 lights: [],
                 models: []
-            }
+            } 
         ];
         this.active_scene = 0;
         this.active_light = 0;
@@ -59,6 +61,73 @@ class Renderer {
             scene.ground_mesh = ground_mesh_callback(scene.scene, scene.ground_subdivisions);
             this['createScene'+ idx](idx);
         });
+    }
+
+    CreateOval(radiusX, radiusY, radialSegments, tubularSegments, scene) {
+        // Initialize arrays for vertex data
+        let positions = [];
+        let indices = [];
+        let normals = [];
+        let uvs = [];
+
+        // Generate vertex data for oval shape
+        for (let i = 0; i <= radialSegments; i++) {
+            let angle = i * Math.PI * 2 / radialSegments;
+            let cosAngle = Math.cos(angle);
+            let sinAngle = Math.sin(angle);
+
+            for (let j = 0; j <= tubularSegments; j++) {
+                let tubularAngle = j * Math.PI * 2 / tubularSegments;
+                let cosTubular = Math.cos(tubularAngle);
+                let sinTubular = Math.sin(tubularAngle);
+
+                let x = radiusX * cosAngle * cosTubular;
+                let y = radiusY * sinAngle * cosTubular;
+                let z = radiusX * sinTubular;
+
+                positions.push(x, y, z);
+
+                let nx = cosAngle * cosTubular;
+                let ny = sinAngle * cosTubular;
+                let nz = sinTubular;
+
+                normals.push(nx, ny, nz);
+
+                let u = j / tubularSegments;
+                let v = i / radialSegments;
+
+                uvs.push(u, v);
+            }
+        }
+
+        // Generate indices for oval shape
+        for (let i = 0; i < radialSegments; i++) {
+            for (let j = 0; j < tubularSegments; j++) {
+                let a = i * (tubularSegments + 1) + j;
+                let b = a + tubularSegments + 1;
+                let c = a + 1;
+                let d = b + 1;
+
+                indices.push(a, b, c);
+                indices.push(b, d, c);
+            }
+        }
+
+        // Create vertex data object
+        let vertexData = new VertexData();
+
+        // Set vertex data arrays
+        vertexData.positions = positions;
+        vertexData.indices = indices;
+        vertexData.normals = normals;
+        vertexData.uvs = uvs;
+
+        // Create mesh object using vertex data
+        let ovalMesh = new Mesh("ovalMesh", scene);
+        vertexData.applyToMesh(ovalMesh);
+
+        // Return mesh object
+        return ovalMesh;
     }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -151,6 +220,71 @@ class Renderer {
         cylinder.material = materials['illum_' + this.shading_alg];
         current_scene.models.push(cylinder);
 
+        scene.onKeyboardObservable.add((kbInfo) => {
+            switch (kbInfo.event.key) {
+                case 'a':
+                  current_scene.lights[this.active_light].position = new Vector3(
+                        current_scene.lights[this.active_light].position.x - 1, current_scene.lights[this.active_light].position.y, current_scene.lights[this.active_light].position.z
+                        );
+                    break;
+                case 'A':
+                  current_scene.lights[this.active_light].position = new Vector3(
+                        current_scene.lights[this.active_light].position.x - 1, current_scene.lights[this.active_light].position.y, current_scene.lights[this.active_light].position.z
+                        );
+                    break;
+                case 'd':
+                    current_scene.lights[this.active_light].position = new Vector3(
+                        current_scene.lights[this.active_light].position.x+1, current_scene.lights[this.active_light].position.y, current_scene.lights[this.active_light].position.z
+                        );
+                    break;
+                case 'D':
+                    current_scene.lights[this.active_light].position = new Vector3(
+                        current_scene.lights[this.active_light].position.x+1, current_scene.lights[this.active_light].position.y, current_scene.lights[this.active_light].position.z
+                        );
+                    break;
+                case 'f':
+                    current_scene.lights[this.active_light].position = new Vector3(
+                        current_scene.lights[this.active_light].position.x, current_scene.lights[this.active_light].position.y-1, current_scene.lights[this.active_light].position.z
+                        );
+                    break;
+                case 'F':
+                    current_scene.lights[this.active_light].position = new Vector3(
+                        current_scene.lights[this.active_light].position.x, current_scene.lights[this.active_light].position.y-1, current_scene.lights[this.active_light].position.z
+                        );
+                    break;
+                case 'r':
+                    current_scene.lights[this.active_light].position = new Vector3(
+                        current_scene.lights[this.active_light].position.x, current_scene.lights[this.active_light].position.y+1, current_scene.lights[this.active_light].position.z
+                        );
+                    break;
+                case 'R':
+                    current_scene.lights[this.active_light].position = new Vector3(
+                        current_scene.lights[this.active_light].position.x, current_scene.lights[this.active_light].position.y+1, current_scene.lights[this.active_light].position.z
+                        );
+                    break;
+                case 'w':
+                    current_scene.lights[this.active_light].position = new Vector3(
+                        current_scene.lights[this.active_light].position.x, current_scene.lights[this.active_light].position.y, current_scene.lights[this.active_light].position.z-1
+                        );
+                    break;
+                case 'W':
+                    current_scene.lights[this.active_light].position = new Vector3(
+                        current_scene.lights[this.active_light].position.x, current_scene.lights[this.active_light].position.y, current_scene.lights[this.active_light].position.z-1
+                        );
+                    break;
+                case 's':
+                    current_scene.lights[this.active_light].position = new Vector3(
+                        current_scene.lights[this.active_light].position.x, current_scene.lights[this.active_light].position.y, current_scene.lights[this.active_light].position.z+1
+                        );
+                    break;
+                case 'S':
+                    current_scene.lights[this.active_light].position = new Vector3(
+                        current_scene.lights[this.active_light].position.x, current_scene.lights[this.active_light].position.y, current_scene.lights[this.active_light].position.z+1
+                        );
+                    break;
+            }
+        });
+
         // Animation function - called before each frame gets rendered
         scene.onBeforeRenderObservable.add(() => {
             // update models and lights here (if needed)
@@ -164,7 +298,7 @@ class Renderer {
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     //*************************************************************************************************************************/
-    //SCENE 1
+    //SCENE 2
     //*************************************************************************************************************************/
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -252,6 +386,71 @@ class Renderer {
         cylinder.material = materials['illum_' + this.shading_alg];
         current_scene.models.push(cylinder);
 
+        scene.onKeyboardObservable.add((kbInfo) => {
+            switch (kbInfo.event.key) {
+                case 'a':
+                  current_scene.lights[this.active_light].position = new Vector3(
+                        current_scene.lights[this.active_light].position.x - 1, current_scene.lights[this.active_light].position.y, current_scene.lights[this.active_light].position.z
+                        );
+                    break;
+                case 'A':
+                  current_scene.lights[this.active_light].position = new Vector3(
+                        current_scene.lights[this.active_light].position.x - 1, current_scene.lights[this.active_light].position.y, current_scene.lights[this.active_light].position.z
+                        );
+                    break;
+                case 'd':
+                    current_scene.lights[this.active_light].position = new Vector3(
+                        current_scene.lights[this.active_light].position.x+1, current_scene.lights[this.active_light].position.y, current_scene.lights[this.active_light].position.z
+                        );
+                    break;
+                case 'D':
+                    current_scene.lights[this.active_light].position = new Vector3(
+                        current_scene.lights[this.active_light].position.x+1, current_scene.lights[this.active_light].position.y, current_scene.lights[this.active_light].position.z
+                        );
+                    break;
+                case 'f':
+                    current_scene.lights[this.active_light].position = new Vector3(
+                        current_scene.lights[this.active_light].position.x, current_scene.lights[this.active_light].position.y-1, current_scene.lights[this.active_light].position.z
+                        );
+                    break;
+                case 'F':
+                    current_scene.lights[this.active_light].position = new Vector3(
+                        current_scene.lights[this.active_light].position.x, current_scene.lights[this.active_light].position.y-1, current_scene.lights[this.active_light].position.z
+                        );
+                    break;
+                case 'r':
+                    current_scene.lights[this.active_light].position = new Vector3(
+                        current_scene.lights[this.active_light].position.x, current_scene.lights[this.active_light].position.y+1, current_scene.lights[this.active_light].position.z
+                        );
+                    break;
+                case 'R':
+                    current_scene.lights[this.active_light].position = new Vector3(
+                        current_scene.lights[this.active_light].position.x, current_scene.lights[this.active_light].position.y+1, current_scene.lights[this.active_light].position.z
+                        );
+                    break;
+                case 'w':
+                    current_scene.lights[this.active_light].position = new Vector3(
+                        current_scene.lights[this.active_light].position.x, current_scene.lights[this.active_light].position.y, current_scene.lights[this.active_light].position.z-1
+                        );
+                    break;
+                case 'W':
+                    current_scene.lights[this.active_light].position = new Vector3(
+                        current_scene.lights[this.active_light].position.x, current_scene.lights[this.active_light].position.y, current_scene.lights[this.active_light].position.z-1
+                        );
+                    break;
+                case 's':
+                    current_scene.lights[this.active_light].position = new Vector3(
+                        current_scene.lights[this.active_light].position.x, current_scene.lights[this.active_light].position.y, current_scene.lights[this.active_light].position.z+1
+                        );
+                    break;
+                case 'S':
+                    current_scene.lights[this.active_light].position = new Vector3(
+                        current_scene.lights[this.active_light].position.x, current_scene.lights[this.active_light].position.y, current_scene.lights[this.active_light].position.z+1
+                        );
+                    break;
+            }
+        });
+
 
         // Animation function - called before each frame gets rendered
         scene.onBeforeRenderObservable.add(() => {
@@ -262,11 +461,13 @@ class Renderer {
             this.updateShaderUniforms(scene_idx, materials['illum_' + this.shading_alg]);
             this.updateShaderUniforms(scene_idx, materials['ground_' + this.shading_alg]);
         });
+
+        
     }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     //*************************************************************************************************************************/
-    //SCENE 1
+    //SCENE 3
     //*************************************************************************************************************************/
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -300,6 +501,12 @@ class Renderer {
         light1.diffuse = new Color3(1.0, 1.0, 1.0);
         light1.specular = new Color3(1.0, 1.0, 1.0);
         current_scene.lights.push(light1);
+
+        let light2 = new PointLight('light2', new Vector3(2.0, 3.0, 3.0), scene);
+        light2.diffuse = new Color3(1.0, 1.0, 1.0);
+        light2.specular = new Color3(1.0, 1.0, 1.0);
+        current_scene.lights.push(light2);
+
 
         // Create ground mesh
         let white_texture = RawTexture.CreateRGBTexture(new Uint8Array([255, 255, 255]), 1, 1, scene);
@@ -352,6 +559,85 @@ class Renderer {
         }
         cylinder.material = materials['illum_' + this.shading_alg];
         current_scene.models.push(cylinder);
+
+        let oval = this.CreateOval(3, 1.5, 32, 16, scene);
+        oval.scaling = new Vector3(0.3, 0.3, 0.3); 
+        oval.position = new Vector3(-5.0, 0.5, 2.0);
+        oval.metadata = {
+            mat_color: new Color3(0.4, 0.9, 0.05),
+            mat_texture: white_texture,
+            mat_specular: new Color3(0.4, 0.4, 0.4),
+            mat_shininess: 4,
+            texture_scale: new Vector2(1.0, 1.0)
+        }
+        oval.material = materials['illum_' + this.shading_alg];
+        current_scene.models.push(oval);
+
+        scene.onKeyboardObservable.add((kbInfo) => {
+            switch (kbInfo.event.key) {
+                case 'a':
+                  current_scene.lights[this.active_light].position = new Vector3(
+                        current_scene.lights[this.active_light].position.x - 1, current_scene.lights[this.active_light].position.y, current_scene.lights[this.active_light].position.z
+                        );
+                    break;
+                case 'A':
+                  current_scene.lights[this.active_light].position = new Vector3(
+                        current_scene.lights[this.active_light].position.x - 1, current_scene.lights[this.active_light].position.y, current_scene.lights[this.active_light].position.z
+                        );
+                    break;
+                case 'd':
+                    current_scene.lights[this.active_light].position = new Vector3(
+                        current_scene.lights[this.active_light].position.x+1, current_scene.lights[this.active_light].position.y, current_scene.lights[this.active_light].position.z
+                        );
+                    break;
+                case 'D':
+                    current_scene.lights[this.active_light].position = new Vector3(
+                        current_scene.lights[this.active_light].position.x+1, current_scene.lights[this.active_light].position.y, current_scene.lights[this.active_light].position.z
+                        );
+                    break;
+                case 'f':
+                    current_scene.lights[this.active_light].position = new Vector3(
+                        current_scene.lights[this.active_light].position.x, current_scene.lights[this.active_light].position.y-1, current_scene.lights[this.active_light].position.z
+                        );
+                    break;
+                case 'F':
+                    current_scene.lights[this.active_light].position = new Vector3(
+                        current_scene.lights[this.active_light].position.x, current_scene.lights[this.active_light].position.y-1, current_scene.lights[this.active_light].position.z
+                        );
+                    break;
+                case 'r':
+                    current_scene.lights[this.active_light].position = new Vector3(
+                        current_scene.lights[this.active_light].position.x, current_scene.lights[this.active_light].position.y+1, current_scene.lights[this.active_light].position.z
+                        );
+                    break;
+                case 'R':
+                    current_scene.lights[this.active_light].position = new Vector3(
+                        current_scene.lights[this.active_light].position.x, current_scene.lights[this.active_light].position.y+1, current_scene.lights[this.active_light].position.z
+                        );
+                    break;
+                case 'w':
+                    current_scene.lights[this.active_light].position = new Vector3(
+                        current_scene.lights[this.active_light].position.x, current_scene.lights[this.active_light].position.y, current_scene.lights[this.active_light].position.z-1
+                        );
+                    break;
+                case 'W':
+                    current_scene.lights[this.active_light].position = new Vector3(
+                        current_scene.lights[this.active_light].position.x, current_scene.lights[this.active_light].position.y, current_scene.lights[this.active_light].position.z-1
+                        );
+                    break;
+                case 's':
+                    current_scene.lights[this.active_light].position = new Vector3(
+                        current_scene.lights[this.active_light].position.x, current_scene.lights[this.active_light].position.y, current_scene.lights[this.active_light].position.z+1
+                        );
+                    break;
+                case 'S':
+                    current_scene.lights[this.active_light].position = new Vector3(
+                        current_scene.lights[this.active_light].position.x, current_scene.lights[this.active_light].position.y, current_scene.lights[this.active_light].position.z+1
+                        );
+                    break;
+            }
+        });
+
 
 
         // Animation function - called before each frame gets rendered
